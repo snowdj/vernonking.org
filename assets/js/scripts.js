4cb043079d42864ec304cab167395fb21a2fe2a7
Christopher Krycho <chris@chriskrycho.com>
2014-01-06 14:52:23 -0500

    Update featured image styles. Closes #10.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -17,13 +17,6 @@ $(document).ready(function() {
 
 });
 
-$(document).ready(function() {
-    var vpH = $(window).height();
-    var vH = vpH - 350;
-    $('.overlay').css("height", vH);
-    $('.featured-image').css("height", vH);
-});
-
 
 $(function(){
   $('<img>').attr('src',function(){
