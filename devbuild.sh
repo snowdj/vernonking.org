2f9be9e1b3447029cfc9e13cb7c3918be1bd55a5
PJ King <github@pjking.net>
2014-01-24 20:38:01 -0500

    post drafts in dev

--- devbuild.sh
+++ devbuild.sh
@@ -1,2 +1,2 @@
 sass --watch assets/sass/main.scss:assets/css/style.css --style compressed &
-jekyll build --watch --config _config.yml,_config-dev.yml
+jekyll build --drafts --watch --config _config.yml,_config-dev.yml &
